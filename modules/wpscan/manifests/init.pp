1a9ae9391bc5974cf739371dd2626b68b81465f9
Auban le Grelle <auban@stag-agency.be>
2017-02-03 01:42:25 +0100

    Update init.pp

--- modules/wpscan/manifests/init.pp
+++ modules/wpscan/manifests/init.pp
@@ -3,25 +3,25 @@ class wpscan {
   #clone rvm
   exec { 'get-rvm':
     require => Class['bootstrap'],
-    cwd => '/vagrant/',
+    cwd => '~',
     command => 'curl -sSL https://rvm.io/mpapis.asc | gpg --import - && curl -sSL https://get.rvm.io | bash -s stable'
   }
   
   exec { 'rvm-ruby-source':
     require => Exec['get-rvm'],
-    cwd => '/vagrant/',
-    command => 'sudo source /vagrant/.rvm/scripts/rvm && echo "source /vagrant/.rvm/scripts/rvm" >> ~/.bashrc'
+    cwd => '~',
+    command => 'source /vagrant/.rvm/scripts/rvm && echo "source /vagrant/.rvm/scripts/rvm" >> ~/.bashrc'
   }
   
   exec { 'rvm-ruby-install':
     require => Exec['rvm-ruby-source'],
-    cwd => '/vagrant/',
-    command => 'sudo rvm install 2.3.3 && sudo rvm use 2.3.3 --default'
+    cwd => '~',
+    command => 'rvm install 2.3.3 && rvm use 2.3.3 --default'
   }
   
   exec { 'gem-install':
     require => Exec['rvm-ruby-install'],
-    cwd => '/vagrant/',
+    cwd => '~',
     command => 'echo "gem: --no-ri --no-rdoc" > /vagrant/.gemrc && sudo gem install bundler'
   }
   
