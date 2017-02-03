0a34ad89886f231f3745e1dc8b6c38d93b1e730c
Auban le Grelle <auban@stag-agency.be>
2017-02-03 02:09:22 +0100

    Update init.pp

--- modules/wpscan/manifests/init.pp
+++ modules/wpscan/manifests/init.pp
@@ -3,26 +3,26 @@ class wpscan {
   #clone rvm
   exec { 'get-rvm':
     require => Class['bootstrap'],
-    cwd => '/home/vagrant/',
+    cwd => '/vagrant/',
     command => 'curl -sSL https://rvm.io/mpapis.asc | gpg --import - && curl -sSL https://get.rvm.io | bash -s stable'
   }
   
   exec { 'rvm-ruby-source':
     require => Exec['get-rvm'],
-    cwd => '/home/vagrant/',
-    command => 'sudo source /home/vagrant/.rvm/scripts/rvm && echo "sudo source /home/vagrant/.rvm/scripts/rvm" >> /home/vagrant/.bashrc'
+    cwd => '/vagrant/',
+    command => 'sudo source /vagrant/.rvm/scripts/rvm && echo "sudo source /vagrant/.rvm/scripts/rvm" >> /vagrant/.bashrc'
   }
   
   exec { 'rvm-ruby-install':
     require => Exec['rvm-ruby-source'],
-    cwd => '/home/vagrant/',
+    cwd => '/vagrant/',
     command => 'sudo rvm install 2.3.3 && sudo rvm use 2.3.3 --default'
   }
   
   exec { 'gem-install':
     require => Exec['rvm-ruby-install'],
-    cwd => '/home/vagrant/',
-    command => 'echo "gem: --no-ri --no-rdoc" > /home/vagrant/.gemrc && sudo gem install bundler'
+    cwd => '/vagrant/',
+    command => 'echo "gem: --no-ri --no-rdoc" > /vagrant/.gemrc && sudo gem install bundler'
   }
   
   #clone wpscan
