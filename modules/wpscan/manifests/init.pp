a13848236ecf143abf9d231d7d429543e0975501
Auban le Grelle <auban@stag-agency.be>
2017-02-04 23:29:56 +0100

    Update init.pp

--- modules/wpscan/manifests/init.pp
+++ modules/wpscan/manifests/init.pp
@@ -1,8 +1,22 @@
 class wpscan {
 
+  #Add Repo
+  exec { 'add-repo':
+    require => Class['bootstrap'],
+    cwd => '/vagrant/',
+    command => 'sudo apt-add-repository ppa:brightbox/ruby-ng && sudo apt-get update'
+  }
+  
+  #Install Ruby 2.3
+  exec { 'ruby-2-3':
+    require => Exec['add-repo'],
+    cwd => '/vagrant/',
+    command => 'sudo apt-get install ruby2.3 ruby2.3-dev'
+  }
+  
   #clone wpscan
   exec { 'clone-wpscan':
-    require => Class['bootstrap'],
+    require => Exec['ruby-2-3'],
     cwd => '/vagrant/',
     command => 'git clone https://github.com/wpscanteam/wpscan.git'
   }
