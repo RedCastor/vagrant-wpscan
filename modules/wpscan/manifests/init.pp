663c78eca9dbf50a9c278d2b388fa41792eebcad
Auban le Grelle <auban@stag-agency.be>
2017-02-04 09:38:21 +0100

    Update init.pp

--- modules/wpscan/manifests/init.pp
+++ modules/wpscan/manifests/init.pp
@@ -1,28 +1,8 @@
 class wpscan {
 
-  #clone rvm
-  exec { 'get-rvm':
-    require => Class['bootstrap'],
-    cwd => '/vagrant/',
-    command => 'sudo apt-get -y install software-properties-common '
-  }
-  
-  exec { 'rvm-ruby-source':
-    require => Exec['get-rvm'],
-    cwd => '/vagrant/',
-    command => 'sudo apt-add-repository ppa:brightbox/ruby-ng'
-  }
-  
-  exec { 'rvm-ruby-install':
-    require => Exec['rvm-ruby-source'],
-    cwd => '/vagrant/',
-    command => 'sudo apt-get -y update && sudo apt-get -y install ruby2.3 ruby2.3-dev'
-  }
-  
-
   #clone wpscan
   exec { 'clone-wpscan':
-    require => Exec['rvm-ruby-install'],
+    require => Class['bootstrap'],
     cwd => '/vagrant/',
     command => 'git clone https://github.com/wpscanteam/wpscan.git'
   }
