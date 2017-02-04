eccf2478dbbbd1b21fccbbcad176cb8bdccb0285
Auban le Grelle <auban@stag-agency.be>
2017-02-04 23:26:36 +0100

    Update init.pp

--- modules/bootstrap/manifests/init.pp
+++ modules/bootstrap/manifests/init.pp
@@ -5,28 +5,22 @@ class bootstrap {
     ensure => 'present'
   }
 
-  # Add brightbox-ruby Repo
-  exec { 'add-brightbox-ruby':
-    command => '/usr/bin/apt-add-repository ppa:brightbox/ruby-ng'
-  }
-  
   # ensure local apt cache index is up to date before beginning
   exec { 'apt-get update':
-    require => Exec['add-brightbox-ruby'],
     command => '/usr/bin/apt-get update'
   }
 
   # package install list
   $packages = [
     "git",
-    "libcurl4-gnutls-dev",
+    "libcurl4-openssl-dev",
     "libxml2",
     "libxml2-dev",
     "libxslt1-dev",
     "ruby-dev",
     "build-essential",
-    "ruby2.3",
-    "ruby2.3-dev",
+    "libgmp-dev",
+    "zlib1g-dev"
   ]
 
   # install packages
