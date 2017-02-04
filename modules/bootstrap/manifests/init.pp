cb1b6dfec868ea9b3f0b8203221376f1c2bd166d
Auban le Grelle <auban@stag-agency.be>
2017-02-04 09:51:04 +0100

    Update init.pp

--- modules/bootstrap/manifests/init.pp
+++ modules/bootstrap/manifests/init.pp
@@ -5,38 +5,27 @@ class bootstrap {
     ensure => 'present'
   }
 
-  # ensure local apt add repo
-  exec { 'apt-add-repository':
-    command => '/usr/bin/apt-add-repository ppa:brightbox/ruby-ng'
-  }
-
   # ensure local apt cache index is up to date before beginning
   exec { 'apt-get update':
-    command => '/usr/bin/apt-get update'
+    command => '/usr/bin/apt-add-repository ppa:brightbox/ruby-ng && /usr/bin/apt-get update'
   }
 
   # package install list
   $packages = [
     "git",
-    "libcurl4-openssl-dev", 
-    "libxml2", 
-    "libxml2-dev", 
-    "libxslt1-dev", 
-    "ruby-dev", 
-    "build-essential", 
-    "libgmp-dev", 
-    "zlib1g-dev",
+    "libcurl4-gnutls-dev",
+    "libxml2",
+    "libxml2-dev",
+    "libxslt1-dev",
     "ruby2.3", 
     "ruby2.3-dev"
+    "build-essential"
   ]
 
   # install packages
   package { $packages:
     ensure => present,
-    require => Exec[
-      "apt-add-repository",
-      "apt-get update"
-    ]
+    require => Exec["apt-get update"]
   }
 
 }
